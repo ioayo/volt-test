eeffb6f989be10eaaea42b6c68da7bd8bcfc66a9
ioayo <bulawake@gmail.com>
2017-10-13 13:08:57 +0800

    hot reload doesnt work

--- src/app.jsx
+++ src/app.jsx
@@ -3,4 +3,4 @@ import { render } from 'react-dom';
 
 import 'react-select/dist/react-select.css';
 
-render(<div>Place your application here</div>, document.getElementById('app-root'));
+render(<div>Place asd фывыфв sadasd</div>, document.getElementById('app-root'));
