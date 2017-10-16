43301185cde0a235e35660321dbf237065a819ff
iocode <bulawake@gmail.com>
2017-10-16 13:42:59 +0800

    edit customer feature

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -22,10 +22,18 @@ class Customers extends React.Component {
 		this.closeCreateModal = this.closeCreateModal.bind(this);
 		this.handleInput = this.handleInput.bind(this);
 		this.createCustomer = this.createCustomer.bind(this);
+		this.showModalEdit = this.showModalEdit.bind(this);
+		this.showModalRemove = this.showModalRemove.bind(this);
+		this.closeEditModal = this.closeEditModal.bind(this);
+		this.editCustomer = this.editCustomer.bind(this);
 	}
 
-	//get customers from api and set state
+	
 	componentDidMount() {
+		this.getCustomers();
+	}
+
+	getCustomers() {
 		fetch('/api/customers')
 			.then(response => response.json())
 			.then(responseData => this.setState({ customers: responseData }))
@@ -42,10 +50,43 @@ class Customers extends React.Component {
 		})
 	}
 	
+	//handle click on edit button 
+	
+	showModalEdit(id) {
+		const customers = this.state.customers;
+		if (id) {
+			const customer = customers.filter(function(customer) {
+  			return customer.id === id;
+			});
+
+			this.setState({
+				showModalEdit: true,
+				customerId: customer[0].id,
+				customerName: customer[0].name,
+				customerAddress: customer[0].address,
+				customerPhone: customer[0].phone,
+			})
+		}
+	}
+
+
+
+	showModalRemove(id) {
+		console.log(id);
+	}
+
 	//handle create modal close
 	closeCreateModal() {
 		this.setState({
 			showModalCreate: false,
+		})
+	}
+
+	//handle edit modal close 
+	
+	closeEditModal() {
+		this.setState({
+			showModalEdit: false
 		})
 	}
 
@@ -60,29 +101,29 @@ class Customers extends React.Component {
 
 	//validate forms
 	validate(data, callback) {
-		const errors = {};
+		let error = false;
     if (!data.name || data.name.trim().length  === 0) {
-    	errors.name = 'error';
+    	error = true;
     	this.setState({ validateName: 'error' })
     } else {
     	this.setState({ validateName: null })
     }
 
     if (!data.address || data.address.trim().length === 0) {
-    	errors.address = 'error';
+    	error = true;
     	this.setState({ validateAddress: 'error' })
     } else {
     	this.setState({ validateAddress: null })
     }
 
     if (!data.phone || data.phone.trim().length === 0) {
-    	errors.phone = 'error';
+    	error = true;
     	this.setState({ validatePhone: 'error' })
     } else {
     	this.setState({ validatePhone: null })
     }
 
-    callback(errors);
+    callback(error);
   }
 
 	//create customer from modal
@@ -99,22 +140,17 @@ class Customers extends React.Component {
 						'address': customerAddress,
 						'phone': customerPhone
 					};
-					
-
+				
 		//validate data and if validate succesful - post new customer to database
 		//
 		this.validate(newCustomer, (validateErrors) => {
-			console.log(validateErrors)
 			//check no errors here
-			if (Object.keys(validateErrors).length === 0) {
-				//add new customer to state
-				let customers = this.state.customers;
-				customers.splice(customers.length, 0, newCustomer);
+			if (!validateErrors) {
 
 				this.setState({ 
-						customers: customers,
 						showModalCreate: false 
 					});
+
 				// POST newCustomer to database
 				fetch('/api/customers/', {
 				  method: 'POST',
@@ -122,23 +158,50 @@ class Customers extends React.Component {
 				    'Accept': 'application/json',
 				    'Content-Type': 'application/json',
 				  },
-				  body: JSON.stringify({
-				  	'id': customerId,
-						'name' : customerName,
-						'address': customerAddress,
-						'phone': customerPhone
-				  })
+				  body: JSON.stringify(newCustomer)
 				})
+				this.getCustomers();
 			} 
 		});
-		
+	}
+
+
+	editCustomer() {
+
+		const customerName = this.state.customerName,
+					customerAddress = this.state.customerAddress,
+					customerPhone = this.state.customerPhone,
+					customerId = this.state.customerId;
+
+		let data = {
+			'id': customerId,
+			'name': customerName,
+			'address': customerAddress,
+			'phone': customerPhone
+		}
+
+		this.validate(data, (validateErrors) => {
+			if (!validateErrors) {
+				this.setState({ showModalEdit: false });
+				console.log(JSON.stringify(data));
+				fetch(`/api/customers/${data.id}`, {
+			    method: 'PUT',
+			    headers: {
+			      'Accept': 'application/json',
+			      'Content-Type': 'application/json'
+			    },
+			    body: JSON.stringify(data),
+		  	});
+		  	
+			}		
+		});
+		this.getCustomers();
 	}
 
 	
 
 
 	render() {
-
 		let customersList = this.state.customers.map((customer) => (
 				<tr key={customer.id}>
 					<td>{customer.id}</td>
@@ -146,8 +209,8 @@ class Customers extends React.Component {
 					<td>{customer.address}</td>
 					<td>{customer.phone}</td>
 					<td>
-						<Button>edit</Button>
-						<Button>remove</Button>
+						<Button onClick={() => this.showModalEdit(customer.id)}>edit</Button>
+						<Button onClick={() => this.showModalRemove(customer.id)}>remove</Button>
 					</td>
 				</tr>
 			))
@@ -210,6 +273,49 @@ class Customers extends React.Component {
           <Modal.Footer>
             <Button bsStyle="primary" onClick={this.createCustomer}>Create</Button>
             <Button onClick={this.closeCreateModal}>Cancel</Button>
+          </Modal.Footer>
+				</Modal>
+
+
+				<Modal show={this.state.showModalEdit} onHide={this.closeEditModal}>
+					<Modal.Header closeButton>
+            <Modal.Title>Edit Customer</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <FormGroup validationState={this.state.validateName}>
+          	<ControlLabel>Name</ControlLabel>
+          	<FormControl 
+							type="text"
+							name="customerName"
+							value={this.state.customerName}
+							onChange={this.handleInput}
+							placeholder="Enter name"
+          	/>
+          </FormGroup>
+          <FormGroup validationState={this.state.validateAddress}>
+          	<ControlLabel>Address</ControlLabel>
+          	<FormControl 
+							type="text"
+							name="customerAddress"
+							value={this.state.customerAddress}
+							onChange={this.handleInput}
+							placeholder="Enter name"
+          	/>
+          </FormGroup >
+          <FormGroup validationState={this.state.validatePhone}>
+          	<ControlLabel>Phone</ControlLabel>
+          	<FormControl 
+							type="text"
+							name="customerPhone"
+							value={this.state.customerPhone}
+							onChange={this.handleInput}
+							placeholder="Enter name"
+          	/>
+          </FormGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="primary" onClick={this.editCustomer}>Edit</Button>
+            <Button onClick={this.closeEditModal}>Cancel</Button>
           </Modal.Footer>
 				</Modal>
 			</div>
